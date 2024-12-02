c28bf13054d585d0c61a4b5326eeb0265ecb5c44
radosimarmata27 <simarmatarado@gmail.com>
2024-12-02 14:25:44 +0700

    add log/now

--- controllers/iologController.js
+++ controllers/iologController.js
@@ -32,7 +32,7 @@ const getIoLogs = async (req, res) => {
         .update({ is_send: true });
     }
     return sendSuccessResponse(res, 200, {
-      ioLogs,
+      log : ioLogs,
       pagination: {
         totalCount,
         totalPages: Math.ceil(totalCount / limit),
@@ -45,4 +45,37 @@ const getIoLogs = async (req, res) => {
   }
 };
 
-module.exports = { getIoLogs };
+const getIoLogsNow = async (req, res) => {
+  try {
+    const now = new Date();    
+    const fiveSecondsAgo = new Date(now.getTime() - 5000);
+    const formattedFiveSecondsAgo = fiveSecondsAgo.toISOString();
+
+    const ioLogs = await db('io_log')
+      .select(
+        'id',
+        'vehicle_name',
+        'imei',
+        'longitude',
+        'latitude',
+        'altitude',
+        'speed',
+        'timestamp',
+      )
+      .where('is_send', false)
+      .andWhere('server_time', '>=', formattedFiveSecondsAgo);
+
+    if (ioLogs.length > 0) {
+      const idsToUpdate = ioLogs.map(log => log.id);
+      await db('io_log')
+        .whereIn('id', idsToUpdate)
+        .update({ is_send: true });
+    }
+    
+    return sendSuccessResponse(res, 200, {log: ioLogs}, 'Logs fetched successfully');
+  } catch (error) {
+    return sendErrorResponse(res, 500, 'Error fetching Logs', error);
+  }
+}
+
+module.exports = { getIoLogs, getIoLogsNow };
--- routes/iologRoutes.js
+++ routes/iologRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getIoLogs } = require('@controllers/iologController');
+const { getIoLogs, getIoLogsNow } = require('@controllers/iologController');
 const authMiddleware = require('@middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getIoLogs);
+router.get('/now', authMiddleware, getIoLogsNow);
 
 module.exports = router;
