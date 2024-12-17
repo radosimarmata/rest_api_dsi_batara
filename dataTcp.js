f97870e024db2f52fc64ee087011ccba4408189a
radosimarmata <simarmatarado@gmail.com>
2024-12-17 17:05:33 +0700

    add log | add interval

--- dataTcp.js
+++ dataTcp.js
@@ -1,11 +1,13 @@
 const net = require('net');
 const moment = require('moment');
-const { db } = require('./config/db');
+const fs = require('fs');
+const path = require('path');
 require('dotenv').config();
+const { db } = require('./config/db');
 
 const HOST_FLEETSIGHT = process.env.HOST_FLEETSIGHT;
 const PORT_FLEETSIGHT = process.env.PORT_FLEETSIGHT;
-const client = new net.Socket();
+
 
 function buildLoginPacket(imei) {
   const startByte = "#L#";  
@@ -41,139 +43,167 @@ function convertToNMEA(degrees, isLatitude) {
   return { coordinate, direction };
 }
 
-const now = moment().utcOffset(8)
-const thirtySecondsAgo = now.subtract(30, 'seconds').format('YYYY-MM-DD HH:mm:ss');
+function logSDPacket(vehicle_name, imei, packet) {
+  const logFolder = path.join(__dirname, 'logs');
+  const logFileName = `${moment().format('YYYY-MM-DD')}.log`;
+  const logFilePath = path.join(logFolder, logFileName);
 
-async function getData(){
-  const ioLogs = await db('io_log')
-    .select(
-      'id',
-      'vehicle_name',
-      'imei',
-      'longitude',
-      'latitude',
-      'altitude',
-      'speed',
-      'timestamp',
-    )
-    .where('is_send', false)
-    .andWhere('timestamp', '>=', thirtySecondsAgo)
-    .orderBy('id', 'DESC');
-  
-  // if (ioLogs.length > 0) {
-  //   const idsToUpdate = ioLogs.map(log => log.id);
-  //   await db('io_log')
-  //     .whereIn('id', idsToUpdate)
-  //     .update({ is_send: true });
-  // }
-  
-  // Set from UTC 8 to UTC 0
-  const formattedLogs = ioLogs.map(log => ({
-    ...log,
-    timestamp: moment(log.timestamp).format('YYYY-MM-DD HH:mm:ss'),
-    date: moment(log.timestamp).utcOffset(-1).format('DDMMYY'),
-    time: moment(log.timestamp).utcOffset(-1).format('HHmmss')
-  }));
+  if (!fs.existsSync(logFolder)) {
+    fs.mkdirSync(logFolder, { recursive: true });
+  }
 
-  await db.destroy();
+  const logEntry = `[${moment().format('YYYY-MM-DD HH:mm:ss')}] [${vehicle_name}] [${imei}] ${packet}\n`;
 
-  // Ambil daftar unik IMEI yang tersedia
-  const uniqueImeiList = [...new Set(formattedLogs.map(log => log.imei))];
-
-  const lastLogsPerImei = uniqueImeiList.map(imei => {
-    // Filter log berdasarkan imei
-    const logsForImei = formattedLogs.filter(log => log.imei === imei);
-    // Urutkan berdasarkan timestamp dan ambil data terakhir
-    const lastLog = logsForImei.sort((a, b) => moment(b.timestamp).diff(moment(a.timestamp)))[0];
-    
-    return lastLog;
+  fs.appendFile(logFilePath, logEntry, (err) => {
+    if (err) {
+      console.error('Error writing log file:', err);
+    } else {
+      console.log('SD Packet logged successfully.');
+    }
   });
-  
-  console.log("Last Logs per IMEI:", lastLogsPerImei);
-
-  lastLogsPerImei.forEach(log => {
-    const imei = log.imei;
-    const loginPacket = buildLoginPacket(imei);
-
-    client.connect(PORT_FLEETSIGHT, HOST_FLEETSIGHT, () => {
-      console.log('Connected to server');
-      client.write(loginPacket);
-    });
-
-    client.on('data', (data) => {
-      const response = data.toString().trim();
-      console.log('Received from server: ', response);
-      if (response === '#AL#1') {
-        console.log('Login successful, sending SD data...');
-        const date = log.date;
-        const time = log.time;
-        const speed = log.speed;
-        const course = 'NA';
-        const alt = log.altitude;
-        const sats = 'NA';
-        const nmeaLatitude = convertToNMEA(log.latitude, true);
-        const nmeaLongitude = convertToNMEA(log.longitude, false);
-        
-        const sdPacket = buildSDPacket(date, time, nmeaLatitude.coordinate, nmeaLatitude.direction, nmeaLongitude.coordinate, nmeaLongitude.direction, speed, course, alt, sats);
-        console.log("SD Packet:", sdPacket);
-        
-        // Kirim paket SD ke server
-        client.write(sdPacket);
-      // Error Login
-      } else if(response === '#AL#0') {
-        console.log('Connection rejected.');
-        client.end();
-      // Error Login
-      } else if(response === '#AL#01'){
-        console.log('Password verification error.');
-        client.end();
-      // Error Login
-      } else if(response === '#AL#10'){
-        console.log('Checksum verification error.');
-        client.end();
-      // Error send SD Packet 
-      } else if(response === '#ASD#-1'){
-        console.log('Incorrect packet structure.');
-        client.end();
-      // Error send SD Packet
-      } else if(response === '#ASD#0'){
-        console.log('Incorrect time.');
-        client.end();
-      // Success send SD Packet
-      } else if(response === '#ASD#1') {
-        console.log('Data SD Packet sent.');
-        client.end();
-      // Error send SD Packet
-      } else if(response === '#ASD#10') {
-        console.log('Error receiving coordinates.');
-        client.end();
-      // Error send SD Packet
-      } else if(response === '#ASD#11') {
-        console.log('Error receiving speed, course, or altitude.');
-        client.end();
-      // Error send SD Packet
-      } else if(response === '#ASD#12') {
-        console.log('Error receiving the number of satellites.');
-        client.end();
-      // Error send SD Packet
-      } else if(response === '#ASD#13') {
-        console.log('Checksum verification error');
-        client.end();
-      } else {
-        console.error('Something wrong :', response);
-        client.end();
-      }
-    });
-
-    client.on('close', () => {
-      console.log('Connection closed');
-    });
-
-    client.on('error', (err) => {
-      console.error('Error occurred:', err.message);
-    });
-  })
-
 }
 
-getData()
+async function getData(){
+  const now = moment().utcOffset(8)
+  const thirtySecondsAgo = now.subtract(30, 'seconds').format('YYYY-MM-DD HH:mm:ss');
+  
+  try {
+    const ioLogs = await db('io_log')
+      .select(
+        'id',
+        'vehicle_name',
+        'imei',
+        'longitude',
+        'latitude',
+        'altitude',
+        'speed',
+        'angle',
+        'satellites',
+        'timestamp'
+      )
+      .where('is_send', false)
+      .andWhere('timestamp', '>=', thirtySecondsAgo)
+      .orderBy('id', 'DESC');
+    
+    // Update ioLog to send is true
+    if (ioLogs.length > 0) {
+      const idsToUpdate = ioLogs.map(log => log.id);
+      await db('io_log')
+        .whereIn('id', idsToUpdate)
+        .update({ is_send: true });
+    }
+    
+    // Set from UTC 8 to UTC 0
+    const formattedLogs = ioLogs.map(log => ({
+      ...log,
+      timestamp: moment(log.timestamp).format('YYYY-MM-DD HH:mm:ss'),
+      date: moment(log.timestamp).utcOffset(-1).format('DDMMYY'),
+      time: moment(log.timestamp).utcOffset(-1).format('HHmmss')
+    }));
+  
+    // Ambil daftar unik IMEI yang tersedia
+    const uniqueImeiList = [...new Set(formattedLogs.map(log => log.imei))];
+  
+    const lastLogsPerImei = uniqueImeiList.map(imei => {
+      const logsForImei = formattedLogs.filter(log => log.imei === imei);
+      const lastLog = logsForImei.sort((a, b) => moment(b.timestamp).diff(moment(a.timestamp)))[0];
+      return lastLog;
+    });
+    
+    lastLogsPerImei.forEach(log => {
+      const client = new net.Socket();
+      const imei = log.imei;
+      const vehicle_name = log.vehicle_name;
+      const loginPacket = buildLoginPacket(imei);
+      let sdPacket
+  
+      client.connect(PORT_FLEETSIGHT, HOST_FLEETSIGHT, () => {
+        console.log('Connected to server');
+        client.write(loginPacket);
+      });
+      
+      client.on('data', (data) => {
+        const response = data.toString().trim();
+        if (response === '#AL#1') {
+          console.log('Login successful, sending SD data..: ', vehicle_name);
+          const date = log.date;
+          const time = log.time;
+          const speed = log.speed;
+          const course = log.angle;
+          const alt = log.altitude;
+          const sats = log.satellites;
+          const nmeaLatitude = convertToNMEA(log.latitude, true);
+          const nmeaLongitude = convertToNMEA(log.longitude, false);
+          
+          sdPacket = buildSDPacket(date, time, nmeaLatitude.coordinate, nmeaLatitude.direction, nmeaLongitude.coordinate, nmeaLongitude.direction, speed, course, alt, sats);
+          console.log("SD Packet:", sdPacket);
+          
+          // Kirim paket SD ke server
+          client.write(sdPacket);
+          
+        // Error Login
+        } else if(response === '#AL#0') {
+          console.log('Connection rejected : ', vehicle_name);
+          client.end();
+        // Error Login
+        } else if(response === '#AL#01'){
+          console.log('Password verification error: ', vehicle_name);
+          client.end();
+        // Error Login
+        } else if(response === '#AL#10'){
+          console.log('Checksum verification error: ', vehicle_name);
+          client.end();
+        // Error send SD Packet 
+        } else if(response === '#ASD#-1'){
+          console.log('Incorrect packet structure: ', vehicle_name);
+          client.end();
+        // Error send SD Packet
+        } else if(response === '#ASD#0'){
+          console.log('Incorrect time: ', vehicle_name);
+          client.end();
+        // Success send SD Packet
+        } else if(response === '#ASD#1') {
+          console.log('Data SD Packet sent: ', vehicle_name);
+          // Log SD Packet ke file
+          logSDPacket(vehicle_name, imei, sdPacket);
+          client.end();
+        // Error send SD Packet
+        } else if(response === '#ASD#10') {
+          console.log('Error receiving coordinates: ', vehicle_name);
+          client.end();
+        // Error send SD Packet
+        } else if(response === '#ASD#11') {
+          console.log('Error receiving speed, course, or altitude: ', vehicle_name);
+          client.end();
+        // Error send SD Packet
+        } else if(response === '#ASD#12') {
+          console.log('Error receiving the number of satellites: ', vehicle_name);
+          client.end();
+        // Error send SD Packet
+        } else if(response === '#ASD#13') {
+          console.log('Checksum verification error: ', vehicle_name);
+          client.end();
+        } else {
+          console.error('Something wrong: ', response);
+          client.end();
+        }
+      });
+  
+      client.on('close', () => {
+        console.log('Connection closed: ', vehicle_name);
+      });
+  
+      client.on('error', (err) => {
+        console.error(`Error occurred ${vehicle_name}:`, err.message);
+      });
+    })
+  } catch (error) {
+    console.error('Error during getData execution:', error.message);
+    await db.destroy();
+  }
+}
+
+setInterval(() => {
+  console.log('Executing at ', moment().format('YYYY-MM-DD HH:mm:ss'))
+  getData();
+}, 15000)   // 15s
