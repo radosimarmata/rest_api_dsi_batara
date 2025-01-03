fa73f08a53f6048dc76db6c8261b30ad8a07f336
radosimarmata <simarmatarado@gmail.com>
2025-01-03 13:48:32 +0700

    add login logout

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -88,7 +88,57 @@ const getPhysicalofAvailability = async (req, res) => {
   }
 }
 
+const getLoginLogout = async (req, res) => {
+  const { database, start_date } = req.query;
+  if(!start_date && !database) {
+    return sendErrorResponse(res, 400, 'Start date & Database name is required');
+  }
+  if (!database) {
+    return sendErrorResponse(res, 400, 'Database name is required');
+  }
+  if (!start_date) {
+    return sendErrorResponse(res, 400, 'Start date is required');
+  }
+  const isValidDate = moment(start_date, 'YYYY-MM-DD', true).isValid();
+  if (!isValidDate) {
+    return sendErrorResponse(res, 400, 'Invalid start date format. Use YYYY-MM-DD');
+  }
+  const dynamicDb = dbconn(`db_${database}`);
+
+  try {
+    const result = await dynamicDb.select(
+      'tanggal as date', 
+      'vehicle_name', 
+      'nrp', 
+      'name', 
+      'type_nrp', 
+      'hour as hmkm',
+      'login as login_time',
+      'login_location',
+      'logout as logout_time',
+      'logout_location'
+    )
+    .from('vw_pbi_login_logout')
+    .where('tanggal', start_date);
+
+    const formattedResult = result.map(row => ({
+      ...row,
+      date: moment(row.date).format('YYYY-MM-DD'),
+    }));
+
+    return sendSuccessResponse(res, 200, 
+      { 
+        message: 'login logout fetched successfully',
+        data: formattedResult
+      }
+    );
+  } catch (error) {
+    return sendErrorResponse(res, 500, 'Error fetching login logout operator', error);
+  }
+}
+
 module.exports = { 
   getUtilizationofAvailability,
-  getPhysicalofAvailability 
+  getPhysicalofAvailability,
+  getLoginLogout
 };
--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
-const { getUtilizationofAvailability, getPhysicalofAvailability } = require('@controllers/reportController');
+const { 
+  getUtilizationofAvailability,
+  getPhysicalofAvailability,
+  getLoginLogout
+} = require('@controllers/reportController');
 const authMiddleware = require('@middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/utilization_of_availability', authMiddleware, getUtilizationofAvailability);
 router.get('/physical_of_availability', authMiddleware, getPhysicalofAvailability);
+router.get('/physical_of_availability', authMiddleware, getPhysicalofAvailability);
+router.get('/login_logout', authMiddleware, getLoginLogout);
 
 module.exports = router;
